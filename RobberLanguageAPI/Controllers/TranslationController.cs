6a7cbf33530baa9a52a5cb3a5f049c4bd45f898f
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-04-21 09:47:13 +0200

    Add TranslationController and Method

--- RobberLanguageAPI/Controllers/TranslationController.cs
+++ RobberLanguageAPI/Controllers/TranslationController.cs
@@ -1,11 +1,56 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using RobberLanguageAPI.Models;
 
 namespace RobberLanguageAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api")]
     [ApiController]
     public class TranslationController : ControllerBase
     {
+
+        [HttpPost]
+        [Route("RobberLanguage")]
+        public Translation PostTranslation(Translation sentence)
+        {
+            var translation = new Translation
+            {
+                OriginalSentance = sentence.OriginalSentance,
+                TranslatedSentance = $"{TranslateSentence(sentence.OriginalSentance)}"
+            };
+
+            return translation;
+
+        }
+
+        private static string TranslateSentence(string sentence)
+        {
+
+            string robberSentence = "";
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == 'a' || sentence[i] == 'e' ||
+                    sentence[i] == 'i' || sentence[i] == 'o' ||
+                    sentence[i] == 'u' || sentence[i] == 'å' ||
+                    sentence[i] == 'ä' || sentence[i] == 'ö')
+                {
+                    robberSentence += sentence[i];
+                }
+                else if (sentence[i] == ' ')
+                {
+                    robberSentence += sentence[i];
+                }
+                else
+                {
+                    robberSentence = robberSentence + sentence[i] + 'o' + sentence[i];
+                }
+            }
+
+            return robberSentence;
+        }
+
+
+
+
     }
 }
